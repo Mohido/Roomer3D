16facd5761172a99e959daa8e5a03ab92c1f1433
Mohammed Al-Mahdawi <m.ghaithalmahdawi@gmail.com>
2024-11-10 10:36:58 +0100

    Added Dynamic Room

--- src/components/3darea/index.tsx
+++ src/components/3darea/index.tsx
@@ -1,12 +1,14 @@
 import  { useEffect, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber'
 import "./index.css";
-import { Box } from './box';
+import { Room } from './room';
+import { OrbitControls, OrthographicCamera } from '@react-three/drei'
+
 
 const Area3D = (props : {fileurl: string}) => {
   const [sceneItems, setSceneItems] = useState<string[]>([]);
   const areaDiv = useRef(null);
-  
+
   const calcSize = () => (window.innerHeight > window.innerWidth)? document.body.clientWidth : document.body.clientHeight;
 
   // Use SideEffect to track fileurl change.
@@ -14,7 +16,7 @@ const Area3D = (props : {fileurl: string}) => {
       if(props.fileurl.length >= 0)
         setSceneItems([...sceneItems, props.fileurl]);
   }, [props.fileurl]);
-  
+
   // Forcing a square shape DivElement
   window.addEventListener('resize', () => {
     if(!areaDiv.current)
@@ -33,11 +35,11 @@ const Area3D = (props : {fileurl: string}) => {
       }} 
     ref={areaDiv}> 
        <Canvas>
+          <OrthographicCamera makeDefault zoom={400} position={[2,2,2]}/>
           <ambientLight intensity={Math.PI / 2} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
+          <directionalLight position={[-10, 10, -10]} intensity={Math.PI} />
+          <Room dimensions={[1,1]}/>
+          <OrbitControls enablePan={false} enableRotate={false} enableDamping={false} enableZoom={true}/>
         </Canvas>
     </div>
   )
