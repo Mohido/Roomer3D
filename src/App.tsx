6940a44ef5fa125c4387ffbb363cf1a749c909ab
Mohammed Al-Mahdawi <48254077+Mohido@users.noreply.github.com>
2024-11-16 10:54:31 +0100

    16th-Nov-2024 Log (#10)
    * Stylized the Modal Menu
    
    * Resizing Room Works
    
    * Implementing Data Serialization

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,18 @@ import Area3D from './components/3darea';
 import { AreaItems } from './components/itemsarea';
 
 
+// A list of the scene objects
 export const AddedObjectContext = createContext<{
   sceneItems: string[];
   addObject: (object3d: string) => void;
   deleteObject: (url_key : string) => void;
+}>(null!);
+
+// Handles the positions of the scene objects
+export const ObjectsPositionsContext = createContext<{
+  positions: {[mesh_id: string] : number[]};        // meshname_123451 -> [0,1]
+  updatePosition_cb: (mesh_id: string, newPos : number[]) => void; // Adds or updates position
+  removePosition_cb: (mesh_id: string) => void;  // Removes a mesh
 }>(null!);
 
 export const ActiveMeshContext = createContext<{
@@ -15,11 +23,34 @@ export const ActiveMeshContext = createContext<{
   setActiveMesh_cb: (uuid: string) => void;    // Takes threejs mesh uuid.
 }>(null!);
 
+export const RoomContext = createContext<{
+  dims: number[];
+  setDims_cb: (dim: number[]) => void;    // Takes threejs mesh uuid.
+}>(null!);
+
 
 export const App = () => {
 
   const [sceneItems, setSceneItems] = useState<string[]>([]);
   const [activeMesh, setActiveMesh] = useState<string>('');
+  const [dims, setDims] = useState<number[]>([4,4]);
+  const [positions, setPos] = useState<{[mesh_id: string] : number[]}>({});
+
+  // TODO: Continue implementing Data Importing and Exporting...
+  const setDims_cb = useCallback((dim:number[]) => {setDims(dim)}, []);
+  const updatePosition_cb = (mesh_id: string, newPos : number[]) => {
+    if(!positions[mesh_id]){
+      setPos({...positions, mesh_id: newPos})
+    }else{
+      const t = positions;
+      t[mesh_id] = newPos;
+      setPos(t);
+    }
+  }; // Adds or updates position
+  const removePosition_cb = (mesh_id: string) => {
+    
+  };  // Removes a mesh
+
 
   // Change objectpath (adds object)
   const setActiveMesh_cb = useCallback((uuid: string) => {setActiveMesh(uuid)}, []);
@@ -44,6 +75,10 @@ export const App = () => {
     setActiveMesh_cb
   }), [activeMesh, setActiveMesh_cb]);
 
+  const RoomContext_ctxt = useMemo(()=>({ 
+    dims,
+    setDims_cb
+  }), [dims, setDims_cb])
 
   return (
     <div className='app'>
@@ -52,7 +87,9 @@ export const App = () => {
       {/* 3D Area */}
       <AddedObjectContext.Provider value={contextValue}>
         <ActiveMeshContext.Provider value={ActivateMesh_ctxt}>
-          <Area3D />
+          <RoomContext.Provider value={RoomContext_ctxt}>
+            <Area3D />
+          </RoomContext.Provider>
         </ActiveMeshContext.Provider>
         {/* Items Area */}
         <AreaItems />
--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -1,14 +1,40 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { FaGear } from "react-icons/fa6";
+import { MdOutlineFileDownload, MdOutlineFileUpload  } from "react-icons/md";
+
 import Modal from 'react-modal';
 import './index.css';
+import { DimInput } from '../dimslider';
+import { RoomContext } from '../../App';
 
 
-// TODO: Make it pretty
-// TODO: Make it functional
+
+// TODO: Uploading and Downloading data...
+// NOTE: Every time the menu opens, the entire component gets re-rendered!
 export const SettingsButton = ()=>{
     const [activeMenu, setActiveMenu] = useState<boolean>(false);
-    
+    const {dims, setDims_cb} = useContext(RoomContext);
+    const dimensions = [dims[0], dims[1]];
+
+    const updateWidth_x = (v:number | number[]) => {
+        if(typeof v == "number")
+            dimensions[0] = v;
+    }
+
+    const updateWidth_y = (v:number | number[]) => {
+        if(typeof v == "number")
+            dimensions[1] = v;
+    }
+
+    const onSubmit = () => {
+        setDims_cb(dimensions);
+        setActiveMenu(false);
+    }
+
+    const onCancel = () => {
+        setActiveMenu(false);
+    }
+
     return <>
         {/* Settings Button */}
         <FaGear onClick={()=> setActiveMenu(true)}  className='button-meta gear-button'/>
@@ -29,35 +55,33 @@ export const SettingsButton = ()=>{
                 </div>
 
                 <div className='subtitle-content'>
-                    <div>
-                        <label htmlFor="dimension">Width</label>
-                        <input name='dimension' type='number'/>
-                    </div>
-
-                    <div>
-                        <label htmlFor="dimension">Depth</label>
-                        <input name='dimension' type='number'/>
-                    </div>
+                    <DimInput defaultState={dimensions[0]} label='Width' desc="Changes room width" onComplete={updateWidth_x} />
+                    <DimInput defaultState={dimensions[1]} label='Length' desc="Changes room length" onComplete={updateWidth_y} />
                 </div>
-                    
-                    
-
 
                 <div className='subtitle-header'>
                     <h2 className='modal-subtitle'>Room Dimensions</h2>
                     <span className='line'></span>
                 </div>
+
                 <div className='subtitle-content'>
-                    <button>Save</button>
-                    <button>Load</button>
+                    <button className='load-button'>
+                        <MdOutlineFileDownload className='button-icon'/>
+                        <span>Save</span>
+                    </button>
+                    <button className='load-button'>
+                        <MdOutlineFileUpload className='button-icon' />
+                        <span>Load</span>
+                    </button>
                 </div>
                     
                 <div className='subtitle-header'>
                     <span className='line'></span>
                 </div>
-                <div className='subtitle-content'>
-                    <button onClick={() => setActiveMenu(false)}>Submit</button>
-                    <button onClick={() => setActiveMenu(false)}>Cancel</button>
+
+                <div className='footer'>
+                    <button className='load-button submit' onClick={onSubmit}>Submit</button>
+                    <button className='load-button cancel' onClick={onCancel}>Cancel</button>
                 </div>
             </Modal>
         </div>
